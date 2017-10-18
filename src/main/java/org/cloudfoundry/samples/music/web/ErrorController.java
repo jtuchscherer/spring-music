dd7244e1cf6c1ed80bc484f2781212ee80fe4a48
Johannes Tuchscherer <jtuchscherer@gmail.com>
2017-10-18 11:36:23 +0200

    Adding oom endpoint

--- src/main/java/org/cloudfoundry/samples/music/web/ErrorController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/ErrorController.java
@@ -21,4 +21,24 @@ public class ErrorController {
         logger.info("Forcing an exception to be thrown");
         throw new NullPointerException("Forcing an exception to be thrown");
     }
+
+    @RequestMapping(value = "/oom")
+    public void throwOOM() throws InterruptedException {
+        logger.info("Forcing an OOM situation");
+        int iteratorValue = 20;
+        logger.info("=================> OOM test started..");
+        for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+            logger.info("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
+            int loop1 = 2;
+            int[] memoryFillIntVar = new int[iteratorValue];
+            // feel memoryFillIntVar array in loop..
+            do {
+                memoryFillIntVar[loop1] = 0;
+                loop1--;
+            } while (loop1 > 0);
+            iteratorValue = iteratorValue * 5;
+            logger.info("\nRequired Memory for next loop: " + iteratorValue);
+            Thread.sleep(1000);
+        }
+    }
 }
